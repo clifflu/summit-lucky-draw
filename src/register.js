ed0eccb24a95475f830b698585be9db3efe302e8
Cliff Chao-kuan Lu <clifflu@gmail.com>
2018-06-25 11:00:23 +0800

    feat: encrypt with KMS

--- src/register.js
+++ src/register.js
@@ -1,50 +1,106 @@
+const AWS = require("aws-sdk");
+const crypto = require("crypto");
+const querystring = require("querystring");
+const util = require("util");
 
-const AWS = require('aws-sdk')
-const querystring = require('querystring')
+const kmsKeyAlias = process.env.kmsKeyAlias;
 
-function extractRoleArn(s) {
-  s = s.trim()
+function checkUgcRoleArn(s) {
+  s = s.trim();
 
   // Use alphanumeric and '+=,.@-_' characters. Maximum 64 characters.
-  let roleRegex = /^arn:aws:iam::\d{12}:role\/[\w\d+=,.@\-_]{1,64}$/
+  let roleRegex = /^arn:aws:iam::\d{12}:role\/[\w\d+=,.@\-_]{1,64}$/;
 
   if (roleRegex.test(s)) {
-    return s
+    return s;
   }
 
-  throw new Error('Invalid Arn')
+  throw new Error("Invalid Arn");
 }
 
-function extractEmail(s) {
-  s = s.trim()
+function checkUgcEmail(s) {
+  s = s.trim();
   if (/^.+@.+$/.test(s)) {
     // valid Email address
-    return s
-  } 
+    return s;
+  }
 
-  throw new Error('Invalid email')
+  throw new Error("Invalid email");
 }
 
 function checkUgc(payload) {
-  let roleArn = extractRoleArn(payload.ugc.roleArn)
-  let email = extractEmail(payload.ugc.email)
+  let input = {
+    roleArn: checkUgcRoleArn(payload.ugc.roleArn),
+    email: checkUgcEmail(payload.ugc.email),
+    when: payload.ugc.when
+  };
 
-  return Object.assign(payload, {input: {roleArn, email}})
+  return Object.assign(payload, { input });
+}
+
+function processInputHmac(src) {
+  let hkey = Buffer.from(process.env.hkey);
+  const hmac = crypto.createHmac("sha256", hkey);
+  hmac.update(src);
+
+  return hmac.digest();
+}
+
+function processInputEncrypt(src) {
+  let kms = new AWS.KMS();
+
+  let params = {
+    KeyId: kmsKeyAlias,
+    Plaintext: Buffer.from(src)
+  };
+
+  return kms
+    .encrypt(params)
+    .promise()
+    .then(data => data.CiphertextBlob);
+}
+
+function processInputAcctId(src) {
+  return src.split(":")[4];
+}
+
+function processInput(payload) {
+  let email = payload.input.email;
+  let roleArn = payload.input.roleArn;
+
+  return Promise.all([
+    processInputHmac(email),
+    processInputEncrypt(email),
+    processInputAcctId(roleArn)
+  ]).then(([hEmail, eEmail, acctId]) =>
+    Object.assign(payload, {
+      item: {
+        HEmail: { B: hEmail },
+        EEmail: { B: eEmail },
+        AcctId: { N: acctId },
+        Submitted: { N: String(payload.input.when) }
+      }
+    })
+  );
 }
 
 function saveRecord(payload) {
-  return payload
+  let ddb = new AWS.DynamoDB();
+
+  console.log(util.inspect({ payload }, { depth: null }));
+  return payload;
 }
 
 function register(evt, ctx) {
-  let tmp = querystring.parse(evt.body)
-  let ugc = {roleArn: tmp.roleArn, email: tmp.email}
+  let tmp = querystring.parse(evt.body);
+  let ugc = { roleArn: tmp.roleArn, email: tmp.email, when: tmp.when };
 
-  return Promise.resolve({ugc})
+  return Promise.resolve({ ugc })
     .then(checkUgc)
-    .then(saveRecord)
+    .then(processInput)
+    .then(saveRecord);
 }
 
 module.exports = {
-  register,
-}
+  register
+};
